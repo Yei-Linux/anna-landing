8f68862cb8fc6e5a3548e443588d7327cc677dd6
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2023-09-12 18:06:42 -0500

    change env var

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import { Fragment } from 'react';
 const AdminWrapper = dynamic(
   () =>
@@ -14,6 +15,20 @@ const AdminWrapper = dynamic(
 const Admin: NextPage = () => {
   return (
     <Fragment>
+      <Head>
+        <title>Anna - Nuestro Asistente virtual</title>
+        <link rel="icon" href="/assets/brand.jpeg" sizes="any" />
+        <meta
+          name="description"
+          content="Anna es un asistente virtual destinado para brinar una guia medica y derivarte segun tu diagnostico a nuestros medicos especializados o general"
+          key="desc"
+        />
+        <meta property="og:title" content="Anna nuestro asistente Virtual" />
+        <meta
+          property="og:description"
+          content="Anna es un asistente virtual destinado para brinar una guia medica y derivarte segun tu diagnostico a nuestros medicos especializados o general"
+        />
+      </Head>
       <AdminWrapper />
     </Fragment>
   );
