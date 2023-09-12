fce76fe0b5d3c51b7c35e44778b6007e4c42c8f3
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2023-09-12 15:58:05 -0500

    feat: Change variables prefix :sparkles:

--- back/config/constants.ts
+++ back/config/constants.ts
@@ -1,8 +1,11 @@
 export const Config = {
-  IS_API_CREATE_DOCTORS_ENABLE: process.env.API_CREATE_DOCTORS_ENABLE === '1',
-  JWT_SALT: Number(process.env.JWT_SALT ?? 10),
-  JWT_SECRET: process.env.JWT_SECRET ?? '',
-  JWT_AUDIENCE: process.env.JWT_AUDIENCE ?? 'urn:example:issuer',
-  JWT_ISSUER: process.env.JWT_ISSUER ?? 'urn:example:audience',
-  JWT_TOKEN_EXPIRATION: Number(process.env.JWT_TOKEN_EXPIRATION ?? 24),
+  IS_API_CREATE_DOCTORS_ENABLE:
+    process.env.NEXT_PUBLIC_API_CREATE_DOCTORS_ENABLE === '1',
+  JWT_SALT: Number(process.env.NEXT_PUBLIC_JWT_SALT ?? 10),
+  JWT_SECRET: process.env.NEXT_PUBLIC_JWT_SECRET ?? '',
+  JWT_AUDIENCE: process.env.NEXT_PUBLIC_JWT_AUDIENCE ?? 'urn:example:issuer',
+  JWT_ISSUER: process.env.NEXT_PUBLIC_JWT_ISSUER ?? 'urn:example:audience',
+  JWT_TOKEN_EXPIRATION: Number(
+    process.env.NEXT_PUBLIC_JWT_TOKEN_EXPIRATION ?? 24
+  ),
 };
