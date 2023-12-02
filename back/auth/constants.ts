c58ef5be1604c484c88ffac0af7928bf0c770e28
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2023-12-02 17:25:54 -0500

    allowDangerousEmailAccountLinking

--- back/auth/constants.ts
+++ back/auth/constants.ts
@@ -2,6 +2,7 @@ const AUTH_PROVIDERS = {
   google: {
     clientId: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    allowDangerousEmailAccountLinking: true,
   },
 };
 const AUTH_SECRET = process.env.SECRET;
