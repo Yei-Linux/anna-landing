6920a9d8edc21d052332ad37a5d8ab44492dc2db
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2023-12-02 17:08:26 -0500

    feat: Add validation on booking :sparkles:

--- src/hooks/useGetBooking.tsx
+++ src/hooks/useGetBooking.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
 import { getBookingAppointments } from '../services/booking-appointment';
 import { TBookingAppointment } from '../components/anna-care/Platform/Appointments/Appointments';
+import { useSession } from 'next-auth/react';
 
 export const useGetBookingAppointments = () => {
+  const { status } = useSession();
   const [bookingAppointments, setBookignAppointments] = useState<
     Array<TBookingAppointment>
   >([]);
 
   const fetchBooking = async () => {
+    if (status !== 'authenticated') return;
     const res = await getBookingAppointments();
     if (!res) return;
 
@@ -16,7 +19,7 @@ export const useGetBookingAppointments = () => {
 
   useEffect(() => {
     fetchBooking();
-  }, []);
+  }, [status]);
 
   return { bookingAppointments };
 };
