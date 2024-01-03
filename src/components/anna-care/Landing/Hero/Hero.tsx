dd48338ea254631b17850068d9fa9ccccd4ca419
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2024-01-03 18:04:28 -0500

    feat: Add main button in home :sparkles:

--- src/components/anna-care/Landing/Hero/Hero.tsx
+++ src/components/anna-care/Landing/Hero/Hero.tsx
@@ -8,12 +8,12 @@ export const Hero = () => {
       <div className="flex flex-col gap-3 px-3 max-w-[500px] m-auto">
         <Text
           text="Adiós complicaciones. Ten bienestar a largo plazo"
-          className="text-center text-primary"
+          className="text-left"
           level="xl"
           as="h3"
         />
         <Text
-          className="text-center text-neutralStrong #8a8a8a"
+          className="text-left text-neutralStrong #8a8a8a"
           text="Finalmente, las familias pueden tratar problemas recurrentes y dolencias crónicas"
           level="base"
         />
@@ -22,12 +22,12 @@ export const Hero = () => {
       <div className="flex flex-col gap-3 px-3 max-w-[500px] m-auto">
         <Text
           text="Orientación todos los días del año."
-          className="text-center text-primary"
+          className="text-left"
           level="xl"
           as="h3"
         />
         <Text
-          className="text-center text-neutralStrong #8a8a8a"
+          className="text-left text-neutralStrong #8a8a8a"
           text="Haz de tu hogar, tu clínica revisando tu progreso de su salud y teniendo acceso a atención crónica como ambulatoria. Sin copagos, tarifas ocultas ni facturas sorpresa."
           level="base"
         />
--- src/components/anna-care/Landing/index.tsx
+++ src/components/anna-care/Landing/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from '../../ui/Button';
+import { useHome } from '../Platform/Home/useHome';
 import { Benefits } from './Benefits/Benefits';
 import { CofounderTestimony } from './CofounderTestimony';
 import { Hero } from './Hero';
@@ -6,6 +8,8 @@ import { Testimonies } from './Testimonies';
 import { Trusters } from './Trusters';
 
 export const Landing = () => {
+  const { handleCarePlus } = useHome();
+
   return (
     <div className="flex flex-col gap-20 md:gap-10">
       <Hero />
@@ -15,6 +19,14 @@ export const Landing = () => {
         <Trusters />
         <CofounderTestimony />
         <Reasons />
+      </div>
+      <div className="w-[100%] flex justify-center fixed bottom-5 z-10 px-4">
+        <Button
+          className="!rounded-full !w-full max-w-[500px] !p-4"
+          onClick={handleCarePlus}
+        >
+          Descubre el plan para ti
+        </Button>
       </div>
     </div>
   );
