850e67613c92c041280e0c95d896d0551716ba7a
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2024-01-24 12:25:41 -0500

    refactor: Change plan prices and benefits :hammer:

--- src/constants/befenits.tsx
+++ src/constants/befenits.tsx
@@ -13,150 +13,149 @@ export const plansDetailsSelected = [
 export const benefitsDetails: any = {
   '656b95997c8c1857eb02447b': [
     {
-      title: 'Consultas médicas virtuales con un Endocrinólogo',
-    },
-    {
-      title: 'Consultas médicas virtuales con un Nutricionista',
-    },
-    {
-      title: 'Seguimiento continuo sobre tu estado médido',
+      title: '3 Doctores de cabezera',
     },
     {
       title:
-        'Hazte tus exámenes de laboratio trimestalmente: Hemograma, Glucosa Basal, Colesterol, Triglicéridos,Urea, Creatinina, Potasio, Sodio y Examen de Orina',
+        'Citas médicas virtuales cada 3 meses con especialistas de Nutrición y 2 especialidades según tu requerimiento',
     },
     {
-      title: 'Obtén tu Glucómetro y tiras reactivas para uso personal',
+      title: 'Envio de consultas de urgencia y rutina 24/7',
+    },
+    {
+      title: 'Grupo de Whatsapp con tu equipo médico',
+    },
+    {
+      title: 'Prueba Trimestral de Descarte crónico',
+    },
+    {
+      title: 'Progreso basado en evidencia médica cada mes (sin costo)',
     },
   ],
   '656b95997c8c1857eb02447c': [
     {
-      title: 'Consultas médicas virtuales con un Cardiólogo',
-    },
-    {
-      title: 'Consultas médicas virtuales con un Nutricionista',
-    },
-    {
-      title: 'Seguimiento continuo sobre tu estado médido',
+      title: '3 Doctores de cabezera',
     },
     {
       title:
-        'Hazte tus exámenes de laboratio trimestalmente: Hemograma, Glucosa Basal, Colesterol, Triglicéridos,Urea, Creatinina, Potasio y Sodio',
+        'Citas médicas virtuales cada 3 meses con especialistas de Nutrición y 2 especialidades según tu requerimiento',
     },
     {
-      title: 'Obtén tu Tensiómetro para cuidado personal',
+      title: 'Envio de consultas de urgencia y rutina 24/7',
     },
     {
-      title:
-        'Ten tu doctor 24/7 para seguimientos menores mediante consultas rápidas',
+      title: 'Grupo de Whatsapp con tu equipo médico',
+    },
+    {
+      title: 'Prueba Trimestral de Descarte crónico',
+    },
+    {
+      title: 'Progreso basado en evidencia médica cada mes (sin costo)',
     },
   ],
   '656b95997c8c1857eb02447d': [
     {
-      title: 'Consultas médicas virtuales con neumológias',
-    },
-    {
-      title: 'Consultas médicas virtuales con Nutricionistas',
-    },
-    {
-      title: 'Seguimiento continuo sobre tu estado médido',
+      title: '3 Doctores de cabezera',
     },
     {
       title:
-        'Hazte tus exámenes de laboratio trimestalmente: Hemograma, Glucosa Basal, Creatina y Urea',
+        'Citas médicas virtuales cada 3 meses con especialistas de Nutrición y 2 especialidades según tu requerimiento',
     },
     {
-      title: 'Ten tu medidor FEM y evalúate siempre',
+      title: 'Envio de consultas de urgencia y rutina 24/7',
     },
     {
-      title:
-        'Ten tu doctor 24/7 para seguimientos menores mediante consultas rápidas',
+      title: 'Grupo de Whatsapp con tu equipo médico',
+    },
+    {
+      title: 'Prueba Trimestral de Descarte crónico',
+    },
+    {
+      title: 'Progreso basado en evidencia médica cada mes (sin costo)',
     },
   ],
   '656b95997c8c1857eb02447e': [
     {
-      title: 'Consultas médicas virtuales con un Endocrinólogo',
-    },
-    {
-      title: 'Consultas médicas virtuales con un Nutricionista',
-    },
-    {
-      title: 'Seguimiento continuo sobre tu estado médido',
-    },
-    {
-      title: 'Comprensión y monitoreo de tu objetivo de salud',
+      title: '3 Doctores de cabezera',
     },
     {
       title:
-        'Hazte tus exámenes de laboratio trimestalmente: Glucosa Basal, Hemograma, Hemoglobina Glicosilada, Colesterol y Triglicéridos, Creatininina, entre otros',
+        'Citas médicas virtuales cada 3 meses con especialistas de Nutrición y 2 especialidades según tu requerimiento',
     },
     {
-      title:
-        'Ten tu doctor 24/7 para seguimientos menores mediante consultas rápidas',
+      title: 'Envio de consultas de urgencia y rutina 24/7',
+    },
+    {
+      title: 'Grupo de Whatsapp con tu equipo médico',
+    },
+    {
+      title: 'Prueba Trimestral de Descarte crónico',
+    },
+    {
+      title: 'Progreso basado en evidencia médica cada mes (sin costo)',
     },
   ],
   '656b95997c8c1857eb02447f': [
     {
-      title: 'Consultas médicas virtuales con un Endocrinólogo',
-    },
-    {
-      title: 'Consultas médicas virtuales con un Nutricionista',
-    },
-    {
-      title: 'Seguimiento continuo sobre tu estado médido',
-    },
-    {
-      title: 'Comprensión y monitoreo de tu objetivo de salud',
+      title: '3 Doctores de cabezera',
     },
     {
       title:
-        'Hazte tus exámenes de laboratio trimestalmente: Glucosa Basal, Hemograma, Hemoglobina Glicosilada, Colesterol y Triglicéridos, Creatininina, entre otros',
+        'Citas médicas virtuales cada 3 meses con especialistas de Nutrición y 2 especialidades según tu requerimiento',
     },
     {
-      title:
-        'Ten tu doctor 24/7 para seguimientos menores mediante consultas rápidas',
+      title: 'Envio de consultas de urgencia y rutina 24/7',
+    },
+    {
+      title: 'Grupo de Whatsapp con tu equipo médico',
+    },
+    {
+      title: 'Prueba Trimestral de Descarte crónico',
+    },
+    {
+      title: 'Progreso basado en evidencia médica cada mes (sin costo)',
     },
   ],
   '656b95997c8c1857eb024480': [
     {
-      title: 'Consultas médicas virtuales con un Endocrinólogo',
-    },
-    {
-      title: 'Consultas médicas virtuales con un Nutricionista',
-    },
-    {
-      title: 'Seguimiento continuo sobre tu estado médido',
-    },
-    {
-      title: 'Comprensión y monitoreo de tu objetivo de salud',
+      title: '3 Doctores de cabezera',
     },
     {
       title:
-        'Hazte tus exámenes de laboratio trimestalmente: Hemograma, T4 libre, TSH, T3, Glucosa Basal, Colesterol, Triglicéridos, Urea, entre otros,',
+        'Citas médicas virtuales cada 3 meses con especialistas de Nutrición y 2 especialidades según tu requerimiento',
     },
     {
-      title:
-        'Ten tu doctor 24/7 para seguimientos menores mediante consultas rápidas',
+      title: 'Envio de consultas de urgencia y rutina 24/7',
+    },
+    {
+      title: 'Grupo de Whatsapp con tu equipo médico',
+    },
+    {
+      title: 'Prueba Trimestral de Descarte crónico',
+    },
+    {
+      title: 'Progreso basado en evidencia médica cada mes (sin costo)',
     },
   ],
   '656b95997c8c1857eb024481': [
     {
-      title: '6 Consultas médicas virtuales en Medicina General',
+      title: '3 Doctores de cabezera',
     },
     {
       title:
-        'Consultas médicas virtuales con Endrocrinólogos, cardiólogos, neumólogos  o nutricionistas',
+        'Citas médicas virtuales cada 3 meses con especialistas de Nutrición y 2 especialidades según tu requerimiento',
     },
     {
-      title: 'Seguimiento continuo sobre tu estado médido',
+      title: 'Envio de consultas de urgencia y rutina 24/7',
     },
     {
-      title:
-        'Hazte tus exámenes de laboratio trimestalmente: Hemograma, Glucosa Basal, Colestero, Triglicéridos, Urea y Creatinina',
+      title: 'Grupo de Whatsapp con tu equipo médico',
     },
     {
-      title:
-        'Ten tu doctor 24/7 para seguimientos menores mediante consultas rápidas',
+      title: 'Prueba Anual de Descarte crónico',
+    },
+    {
+      title: 'Progreso basado en evidencia médica cada mes (sin costo)',
     },
   ],
 };
